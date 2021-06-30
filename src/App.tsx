755c0b64b96d3f7babfe627d4f9566ab287d92a9
Juan Gremes <gremjua@gmail.com>
2021-06-30 07:57:53 -0300

    add responsive and collapsable app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from 'logo.svg';
 import 'App.css';
 import NavBar from 'components/navBar';
+import Button from '@material-ui/core/Button';
 
 function App(): JSX.Element {
+	const [headerCollapsed, setHeaderCollapsed] = useState(true);
+	const collapseHeader = () => setHeaderCollapsed(!headerCollapsed);
 	return (
 		<div className='App'>
-			<NavBar />
-			<header className='App-header'>
+			<NavBar collapse={headerCollapsed} />
+			<Button onClick={collapseHeader}>Collapse</Button>
+			<div className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
@@ -20,7 +24,53 @@ function App(): JSX.Element {
 				>
 					Learn React
 				</a>
-			</header>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</div>
+			{/* pretzel y pretzel 3 */}
+			<div>
+				Icons made by{' '}
+				<a href='https://www.freepik.com' title='Freepik'>
+					Freepik
+				</a>{' '}
+				from{' '}
+				<a href='https://www.flaticon.com/' title='Flaticon'>
+					www.flaticon.com
+				</a>
+			</div>
+			{/* pretzel 2 */}
+			<div>
+				Icons made by{' '}
+				<a href='https://www.flaticon.com/authors/those-icons' title='Those Icons'>
+					Those Icons
+				</a>{' '}
+				from{' '}
+				<a href='https://www.flaticon.com/' title='Flaticon'>
+					www.flaticon.com
+				</a>
+			</div>
 		</div>
 	);
 }
--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,50 +1,56 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import Collapse from '@material-ui/core/Collapse';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
 import ElevationScroll from 'components/navBar/ElevationScroll';
 import Fab from '@material-ui/core/Fab';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import ScrollTop from 'components/navBar/ScrollTop';
+import headerImage from 'images/juanYSol.jpg';
+import pretzel from 'images/pretzel3.svg';
+import './index.css';
 
-interface Props {
+type NavBarProps = {
 	/**
 	 * Injected to work in an iframe.
 	 */
 	window?: () => Window;
 	children?: React.ReactElement;
-}
+	collapse: boolean;
+};
 
-const NavBar = (props: Props): JSX.Element => (
-	<>
-		<ElevationScroll {...props}>
-			<AppBar>
-				<Toolbar>
-					<Typography variant='h6'>Scroll to Elevate App Bar</Typography>
-				</Toolbar>
-			</AppBar>
-		</ElevationScroll>
-		<Toolbar id='back-to-top-anchor' />
-		<Container>
-			<Box my={2}>
-				{[...new Array(12)]
-					.map(
-						() => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-					)
-					.join('\n')}
-			</Box>
-		</Container>
-		<ScrollTop {...props}>
-			<Fab color='secondary' size='small' aria-label='scroll back to top'>
-				<KeyboardArrowUpIcon />
-			</Fab>
-		</ScrollTop>
-	</>
-);
+const NavBar = (props: NavBarProps): JSX.Element => {
+	const { collapse } = props;
+	return (
+		<>
+			<div id='back-to-top-anchor' />
+			<ElevationScroll {...props}>
+				<AppBar position={collapse ? 'relative' : 'fixed'}>
+					{/* here we should remove position and see how material ui does with padding etc */}
+					<Box className='headerContainer'>
+						<Toolbar>
+							<Typography variant='h6'>Juan & Sol</Typography>
+						</Toolbar>
+						<Collapse in={collapse}>
+							<Box className='headerImageContainer'>
+								<img src={headerImage} alt='Juan and Sol' className='headerImage' />
+							</Box>
+						</Collapse>
+						<Box className='logoContainer'>
+							<img src={pretzel} alt='Pretzel' className='logo' />
+						</Box>
+					</Box>
+				</AppBar>
+			</ElevationScroll>
+			<ScrollTop {...props}>
+				<Fab color='primary' size='small' aria-label='scroll back to top'>
+					<KeyboardArrowUpIcon />
+				</Fab>
+			</ScrollTop>
+		</>
+	);
+};
 
 export default NavBar;
