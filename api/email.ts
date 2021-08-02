74f012e5a163e2fb918b01e4229bd8e8af9f6567
Juan Gremes <jgremes@Juans-MacBook-Pro.local>
2021-08-02 19:05:52 -0400

    add email and nodemailer

--- api/email.ts
+++ api/email.ts
@@ -1,6 +1,30 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import { EmailData } from 'net/email';
+import { createTransport } from 'nodemailer';
+import { MailOptions } from 'nodemailer/lib/json-transport';
 
 export default (request: VercelRequest, response: VercelResponse): void => {
-	const { name = 'World' } = request.query;
-	response.status(200).send({ data: `Hello ${name}!` });
+	const emailData: EmailData = request.body;
+
+	const transporter = createTransport({
+		service: 'gmail',
+		auth: {
+			user: process.env.REACT_APP_EMAIL,
+			pass: process.env.REACT_APP_EMAIL_PASS,
+		},
+	});
+	const mailOptions: MailOptions = {
+		from: emailData.from,
+		to: emailData.to,
+		subject: emailData.subject,
+		html: emailData.content,
+	};
+
+	transporter.sendMail(mailOptions, (err, info) => {
+		if (err) {
+			response.status(500).send({ data: 'Error sending email' });
+		} else {
+			response.status(200).send({ data: 'Email sent correctly' });
+		}
+	});
 };
--- src/net/email.ts
+++ src/net/email.ts
@@ -4,21 +4,70 @@ export type EmailData = {
 	from: string;
 	to: string;
 	content: string;
+	subject: string;
 };
 
 const buildEmail = (transaction: Transaction, id: string): EmailData => {
-	const from = 'pretzels@gmail.com';
+	const from = process.env.REACT_APP_EMAIL || '';
 	const { email: to } = transaction;
-	const content = `&#127881; ¡Confirmaste tu regalo a través de Pretzels! &#127881;\n
-    Ahora solo tenés que hacer la transferencia por un monto de $${transaction.amount.toLocaleString(
-					'es-ar'
-				)} a la cuenta de Juan y Sol:\n
-                \tCBU: 0720169788000001382190\n
-                \tAlias de CBU: VOLCAN.JARDIN.ABRIL\n\n
-    Después subí el comprobante en este link: ${
-					process.env.VERCEL_URL
-				}/upload/${id}`;
-	return { from, to, content };
+
+	const content = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+      <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <!-- NOTE: external links are for testing only -->
+        <link href="//cdn.muicss.com/mui-0.10.3/email/mui-email-styletag.css" rel="stylesheet" />
+        <link href="//cdn.muicss.com/mui-0.10.3/email/mui-email-inline.css" rel="stylesheet" />
+      </head>
+      <body>
+        <table class="mui-body" cellpadding="0" cellspacing="0" border="0">
+          <tr>
+            <td>
+              <center>
+                <!--[if mso]><table><tr><td class="mui-container-fixed"><![endif]-->
+                <div class="mui-container">
+                    <div class="mui--text-headline">
+                        &#127881; ¡Confirmaste tu regalo! &#127881;
+                    </div>
+                    <div class="mui--text-body1">
+                        El ID de tu regalo es: <b>${id}</b>.<br />
+                        Hacé la transferencia de <b>$${transaction.amount}</b> a la siguiente cuenta y recordá <b>subir el comprobante</b>:
+                        <br />
+                        <b>
+                            <i>Banco Santander</i>
+                        </b>
+                        <br />
+                        <b>Tipo y número de cuenta</b>: Cuenta en Pesos 169-013821/9
+                        <br />
+                        <b>Número de CBU</b>:
+                        <input
+                            readOnly
+                            value='0720169788000001382190'
+                            style="width:172px; border:none;"
+                        />
+                        <br />
+                        <b>Alias de CBU</b>: VOLCAN.JARDIN.ABRIL
+                        <br />
+                        <b>Titular de la cuenta</b>: Gremes Cordero Juan Agustin
+                        <br />
+                        <b>Tipo y número de documento</b>: DNI 38028338
+                    </div>
+                    <div class="mui--text-button">
+                        <a href="${process.env.REACT_APP_VERCEL_URL}/upload/${id}">
+                        Subí el comprobante de transferencia
+                        </a>
+                    </div>
+                </div>
+                <!--[if mso]></td></tr></table><![endif]-->
+              </center>
+            </td>
+          </tr>
+        </table>
+      </body>
+    </html>`;
+	const subject = '🎉  Confirmaste tu regalo para Juan y Sol 🎉 ';
+	return { from, to, content, subject };
 };
 
 export const sendEmail = async (
